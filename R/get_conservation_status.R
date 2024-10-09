48a83fd4084508393c78938e6e33999d51a8a03d
PaulESantos <paulefrens@gmail.com>
2024-10-08 23:01:19 -0500

    v24-1

--- R/get_conservation_status.R
+++ R/get_conservation_status.R
@@ -9,7 +9,7 @@
 
 get_conservation_status <- function(species_names) {
   # Convertir el tibble iucn_data a data.table para optimización
-  data.table::as.data.table(iucn_data)
+  data.table::as.data.table(iucn_2024)
   # Validar entrada
   if (missing(species_names) || !is.character(species_names) || length(species_names) == 0) {
     stop("Please provide a non-empty character vector of species names.")
@@ -23,10 +23,10 @@ get_conservation_status <- function(species_names) {
 
   # Búsqueda y asignación de estados de conservación
   for (i in seq_along(species_names_lower)) {
-    match_index <- which(tolower(iucn_data$search_name) == species_names_lower[i])  # Convertir a minúsculas para la comparación
+    match_index <- which(tolower(iucn_2024$scientific_name) == species_names_lower[i])  # Convertir a minúsculas para la comparación
 
     if (length(match_index) > 0) {
-      conservation_status_vector[i] <- iucn_data$threat_status[match_index][1]  # Obtener el primer estado de conservación
+      conservation_status_vector[i] <- iucn_2024$threat_status[match_index][1]  # Obtener el primer estado de conservación
     } else {
       conservation_status_vector[i] <- "no match found"  # Asignar mensaje de no coincidencia
     }
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,39 @@
 utils::globalVariables(c(
   "iucn_data", "search_name",
   ".", ":=", "core_id", "submitted_name", "vernacular_name",
-  "search_name_lower"
+  "search_name_lower","iucn_2024"
 ))
+
+# ---------------------------------------------------------------
+
+
+.onAttach <- function(lib, pkg) {
+  packageStartupMessage("This is iucnr ",
+                        utils::packageDescription("iucnr",
+                                                  fields = "Version"
+                        ),
+                        "\nThe iucnr package is compatible with IUCN Red List version 2024-1,\nproviding tools to access and analyze the latest conservation status data.",
+                        appendLF = TRUE
+  )
+}
+
+
+# -------------------------------------------------------------------------
+
+show_progress <- function() {
+  isTRUE(getOption("iucnr.show_progress")) && # user disables progress bar
+    interactive() # Not actively knitting a document
+}
+
+
+
+.onLoad <- function(libname, pkgname) {
+  opt <- options()
+  opt_iucnr <- list(
+    iucnr.show_progress = TRUE
+  )
+  to_set <- !(names(opt_iucnr) %in% names(opt))
+  if (any(to_set)) options(opt_iucnr[to_set])
+  invisible()
+}
+
